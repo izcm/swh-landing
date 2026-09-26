8e5a9a01ac3187882fb499917f994c2f2d066eb1
izcm <izcm@izblocks.com>
2026-09-26 10:00:52 +0200

    favicon + site title

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,27 +5,33 @@ type NavTab = {
 
 export default function Nav({ tabs }: { tabs: NavTab[] }) {
   return (
-    <header className="flex items-center justify-between gap-8 my-3 px-8">
-      <span className="flex-1 text-lg font-semibold tracking-wide text-fg">
-        SWH
-      </span>
-      {/* <nav className="items-center gap-8 text-sm text-subtle flex">
+    <>
+      <div className="bg-warning py-1.5 text-center text-xs font-semibold tracking-widest text-ground">
+        CURRENTLY UNDER DEVELOPMENT
+      </div>
+
+      <header className="flex items-center justify-between gap-8 my-3 px-8">
+        <span className="flex-1 text-lg font-semibold tracking-wide text-fg">
+          SWH
+        </span>
+        {/* <nav className="items-center gap-8 text-sm text-subtle flex">
         {tabs.map((tab) => (
           <a key={tab.hash} href={`#${tab.hash}`} className="hover:text-fg">
             {tab.label}
           </a>
         ))}
       </nav> */}
-      <a href="#demo" className="btn btn-menu text-fg text-sm">
-        Live Demo
-      </a>
+        <a href="#demo" className="btn btn-menu text-fg text-sm">
+          Live Demo
+        </a>
 
-      <a
-        href="#contact"
-        className="btn btn-menu border border-accent/40 text-sm"
-      >
-        Get in touch
-      </a>
-    </header>
+        <a
+          href="#contact"
+          className="btn btn-menu border border-accent/40 text-sm"
+        >
+          Get in touch
+        </a>
+      </header>
+    </>
   );
 }
